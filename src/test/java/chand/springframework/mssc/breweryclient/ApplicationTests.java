c64923fe452a0c601e93a1d5387c29e997dc2fe8
chandana-tech <scb4java@yahoo.com>
2020-10-15 22:50:53 +0530

    created the spring boot initial project

---
+++ src/test/java/chand/springframework/mssc/breweryclient/ApplicationTests.java
@@ -0,0 +1,13 @@
+package chand.springframework.mssc.breweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
